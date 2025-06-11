762afc366a325ecf591b3d1f102c0544d2f5a7ec
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-06-11 12:27:50 +0200

    Flake8 corrections

--- backend/app/db/cassandra.py
+++ backend/app/db/cassandra.py
@@ -1,19 +1,24 @@
 from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
+# from cassandra.auth import PlainTextAuthProvider
+
 
 cluster = None
 session = None
 
+
 async def init_cassandra():
     global cluster, session
-    cluster = Cluster(['127.0.0.2', '127.0.0.3'])  # add more IPs for multiple nodes
+    # add more IPs for multiple nodes
+    cluster = Cluster(['127.0.0.2', '127.0.0.3'])
     session = cluster.connect('data')
     print("Connected to Cassandra")
+
 
 async def close_cassandra():
     if cluster:
         cluster.shutdown()
         print("Cassandra connection closed")
+
 
 def get_session():
     return session
