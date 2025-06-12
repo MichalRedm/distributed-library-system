f885fc1239d3b201f27cf2cac5275672117385ab
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-06-12 16:01:02 +0200

    Book list component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,45 @@ import { useState } from "react";
 import "./App.scss";
 import User from "./components/User";
 import UserList from "./components/UserList";
+import BookList from "./components/BookList";
 
 function App() {
   const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
+  const [selectedBookId, setSelectedBookId] = useState<number | null>(null);
+  const [tab, setTab] = useState<"users" | "books">("users");
 
   const handleSelectUserId = (userId: number) => {
     setSelectedUserId(userId);
   };
 
+  const handleSelectBookId = (bookId: number) => {
+    setSelectedBookId(bookId);
+  };
+
   return (
     <div className="App">
-      <UserList onSelectUser={handleSelectUserId} />
+      <div>
+        <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
+          <button
+            className={`tab-button ${tab === "users" ? "active" : ""}`}
+            onClick={() => setTab("users")}
+          >
+            Users
+          </button>
+          <button
+            className={`tab-button ${tab === "books" ? "active" : ""}`}
+            onClick={() => setTab("books")}
+          >
+            Books
+          </button>
+        </div>
+        {tab === "users" && (
+          <UserList onSelectUser={handleSelectUserId} />
+        )}
+        { tab === "books" && (
+          <BookList onSelectBook={handleSelectBookId} />
+        )}
+      </div>
       <User id={selectedUserId} />
     </div>
   );
