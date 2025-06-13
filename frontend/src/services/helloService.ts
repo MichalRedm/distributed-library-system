c0cc7d990610a5ecf6ba5005ace30b1d8d650ea7
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-06-14 00:06:13 +0200

    Delete helloService.ts

--- frontend/src/services/helloService.ts
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-import type { HelloResponse } from "../types/hello";
-
-export async function fetchHello(): Promise<HelloResponse> {
-  const response = await api.get<HelloResponse>("/hello");
-  return response.data;
-}
